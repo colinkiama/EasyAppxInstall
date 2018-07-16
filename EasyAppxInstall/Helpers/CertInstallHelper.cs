76f6134b6e178e6e22f3a81a63e16fe0c7db2583
Colin Kiama <colinkiama@hotmail.co.uk>
2018-07-16 19:19:28 +0100

    Starting to add package deployment class

---
+++ EasyAppxInstall/Helpers/CertInstallHelper.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyAppxInstall.Helpers
+{
+    public class CertInstallHelper
+    {
+        public static void InstallCert(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string filePath = args[0];
+                bool doesFileExist = System.IO.File.Exists(filePath);
+                if (doesFileExist)
+                {
+                    string fileExtension = filePath.Split('.')[1];
+                    if (fileExtension == ".cer")
+                    {
+                        InstallCertFromCertFilePath(filePath);
+                    }
+                    else if (fileExtension == ".appx" || fileExtension == ".appxbundle")
+                    {
+                        InstallCertFromSignedPackage(filePath);
+                    }
+                }
+                else if (Directory.Exists(args[0]))
+                {
+                    InstallCertFromDirectory(args[0]);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid parameters were entered. Please enter a path to a certificate file \".cer\" app package file \".appx\" or \".appxbundle\" or a directory");
+                }
+            }
+
+            else
+            {
+                string currentDirectory = Directory.GetCurrentDirectory();
+                InstallCertFromDirectory(currentDirectory);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void InstallCertFromDirectory(string currentDirectory)
+        {
+            Console.WriteLine($"Current directory is: {currentDirectory}");
+            var files = Directory.GetFiles(currentDirectory, "*.cer");
+
+
+            if (files.Length > 0)
+            {
+                string certFileName = files.First();
+                X509Certificate2 cert = new X509Certificate2(certFileName);
+                using (X509Store store = new X509Store(StoreName.TrustedPeople, StoreLocation.LocalMachine))
+                {
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Add(cert);
+                }
+                Console.WriteLine($"{certFileName} has successfully been added to the certifcate store.");
+                Console.WriteLine("You can now easily install the app by double clicking the .appx/.appxbundle file");
+            }
+
+            else
+            {
+                InstallCertFromSignedPackageFromDirectory();
+            }
+
+
+        }
+
+        public static void InstallCertFromSignedPackage(string filePath)
+        {
+            PrintFilePath(filePath);
+            X509Certificate certFromSignedPkg = X509Certificate.CreateFromSignedFile(filePath);
+            X509Certificate2 cert = new X509Certificate2(certFromSignedPkg);
+            using (X509Store store = new X509Store(StoreName.TrustedPeople, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(cert);
+            }
+            Console.WriteLine($"Certificate for signed package: {filePath} has successfully been added to the certifcate store.");
+            Console.WriteLine("You can now easily install the app by double clicking the .appx/.appxbundle file");
+        }
+
+        public static void InstallCertFromCertFilePath(string filePath)
+        {
+            PrintFilePath(filePath);
+            X509Certificate2 cert = new X509Certificate2(filePath);
+            using (X509Store store = new X509Store(StoreName.TrustedPeople, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(cert);
+            }
+            Console.WriteLine($"{filePath} has successfully been added to the certifcate store.");
+            Console.WriteLine("You can now easily install the app by double clicking the .appx/.appxbundle file");
+        }
+
+        private static void PrintFilePath(string filePath)
+        {
+            Console.WriteLine($"Current file path: {filePath}");
+        }
+
+        private static void InstallCertFromSignedPackageFromDirectory()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string signedPackagePath = TryFindSignedPackagePath(currentDirectory);
+
+
+            if (signedPackagePath != "null")
+            {
+                InstallCertFromSignedPackage(signedPackagePath);
+            }
+
+            else
+            {
+                Console.WriteLine("Certificate file or signed package is not available in the current directory.");
+                Console.WriteLine("Please launch this program in the directory where the certifcate file or appx  you want to install is located");
+            }
+        }
+
+
+
+        private static string TryFindSignedPackagePath(string currentDirectory)
+        {
+            string packagePath = "null";
+
+            Tuple<bool, string> fileQueryResult = FindFilesFromDirectory(currentDirectory, "*.appxbundle");
+            if (fileQueryResult.Item1 == true)
+            {
+                packagePath = fileQueryResult.Item2;
+            }
+
+            else
+            {
+                fileQueryResult = FindFilesFromDirectory(currentDirectory, "*.appx");
+                if (fileQueryResult.Item1 == true)
+                {
+                    packagePath = fileQueryResult.Item2;
+                }
+
+            }
+
+            return packagePath;
+        }
+
+        private static Tuple<bool, string> FindFilesFromDirectory(string currentDirectory, string searchPattern)
+        {
+            string fileName = "";
+            bool isFileFound = false;
+
+            var files = Directory.GetFiles(currentDirectory, searchPattern);
+            if (files.Length > 0)
+            {
+                isFileFound = true;
+                fileName = files[0];
+            }
+
+            return Tuple.Create(isFileFound, fileName);
+        }
+    }
+}
---
+++ EasyAppxInstall/Helpers/PackageInstallHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Management.Deployment;
+using Windows.Storage;
+
+
+namespace EasyAppxInstall.Helpers
+{
+   public class PackageInstallHelper
+    {
+        StorageFile packageInContext;
+        List<Uri> dependencies = new List<Uri>();
+        //ValueSet cannot contain values of the URI class which is why there is another list below.
+        //This is required to update the progress in a notification using a background task.
+        List<string> dependenciesAsString = new List<string>();
+
+        bool pkgRegistered = false;
+
+        private async void showProgressInApp()
+        {
+          
+            PackageManager pkgManager = new PackageManager();
+            Progress<DeploymentProgress> progressCallback = new Progress<DeploymentProgress>(installProgress);
+            string resultText = "Nothing";
+
+            
+            if (dependencies != null && dependencies.Count > 0)
+            {
+                try
+                {
+                    var result = await pkgManager.AddPackageAsync(new Uri(packageInContext.Path), dependencies, DeploymentOptions.ForceTargetApplicationShutdown).AsTask(progressCallback);
+                    checkIfPackageRegistered(result, resultText);
+
+                }
+                catch (Exception e)
+                {
+                    resultText = e.Message;
+                }
+
+            }
+            else
+            {
+                try
+                {
+
+                    var result = await pkgManager.AddPackageAsync(new Uri(packageInContext.Path), null, DeploymentOptions.ForceTargetApplicationShutdown).AsTask(progressCallback);
+                    checkIfPackageRegistered(result, resultText);
+                }
+
+                catch (Exception e)
+                {
+                    resultText = e.Message;
+                }
+
+            }
+
+          
+            if (pkgRegistered == true)
+            {
+                permissionTextBlock.Text = "Completed";
+                notification.ShowInstallationHasCompleted(packageInContext.Name);
+
+
+
+            }
+            else
+            {
+                resultTextBlock.Text = resultText;
+                notification.sendError(resultText);
+            }
+        }
+
+        private void checkIfPackageRegistered(DeploymentResult result, string resultText)
+        {
+            if (result.)
+            {
+
+            }
+            {
+                resultText = result.ErrorText;
+            }
+        }
+
+       
+        private void installProgress(DeploymentProgress installProgress)
+        {
+            double installPercentage = installProgress.percentage;
+            string percentageAsString = String.Format($"{installPercentage}%");
+        }
+
+        
+
+        
+
+    }
+}
